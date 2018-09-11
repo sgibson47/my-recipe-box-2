fb10ea04ec6d345f394632c3bb9a36f4541a53b6
sgibson47 <sgibson47@gmail.com>
2018-09-11 12:56:33 -0500

    add logic to users#create action to make a new instance of User with attributes from the user, redirect to the new user's show page if the new user is successfully saved, and render the users 'new' view if not

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,12 @@ class UsersController < ApplicationController
   end
 
   def create
-    
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to user_path(@user)
+    else
+      render "new"
+    end
   end
 
   def show
