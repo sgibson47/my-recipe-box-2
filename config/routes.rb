85c5ed5d3402012215f52a4d75006c9d9e5247d6
sgibson47 <sgibson47@gmail.com>
2018-09-11 17:53:23 -0500

    draw ingredients routes with resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
   root 'welcome#home'
 
-  resources :users, only: [:new, :create, :show]
+  resources :users, only: [:new, :create, :show, :delete]
 
   get '/signin', to: 'sessions#new'
 
   post '/signin', to: 'sessions#create'
 
   delete "/signout", to: "sessions#destroy"
+
+  resources :ingredients
 end
