0e6f909e9137404f0c71cdd6b95505c8ce733bcc
sgibson47 <sgibson47@gmail.com>
2018-09-11 13:44:37 -0500

    draw routes to sessions new, create and destroy

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,10 @@ Rails.application.routes.draw do
   root 'welcome#home'
 
   resources :users, only: [:new, :create, :show]
+
+  get '/signin', to: 'sessions#new'
+
+  post '/signin', to: 'sessions#create'
+
+  delete "/signout", to: "sessions#destroy"
 end
