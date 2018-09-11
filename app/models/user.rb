452893acabc2e705faa3c7facf702447cf950fb3
sgibson47 <sgibson47@gmail.com>
2018-09-11 13:07:22 -0500

    add validations to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,9 @@
 class User < ApplicationRecord
   has_secure_password
 
+  validates :name, :email, presence: true
+  validates :password, confirmation: true
+  validates :email, uniqueness: true 
+
   has_many :recipes
 end
