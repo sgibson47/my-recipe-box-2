dac53d09c8f8d5a203fb4704ce59be5411b382ef
sgibson47 <sgibson47@gmail.com>
2018-09-11 13:57:18 -0500

    add logic to create action

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,4 +2,15 @@ class SessionsController < ApplicationController
 
   def new
   end
+
+  def create
+    @user = User.find_by(email: params[:email])
+    if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      redirect_to user_path(@user)
+    else
+      flash[:message] = "Invalid email and password combination."
+      redirect_to '/signin'
+    end
+  end
 end
