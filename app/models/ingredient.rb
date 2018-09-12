7d223325a608f09728111f7d78ef9087512aa007
sgibson47 <sgibson47@gmail.com>
2018-09-12 18:44:59 -0500

    define #name_with_serving_info

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,4 +1,8 @@
 class Ingredient < ApplicationRecord
   has_many :amounts
   has_many :recipes, :through => :amounts
+
+  def name_with_serving_info
+    "#{self.name} #{self.serving_size_number} #self. serving_size_unit}"
+  end
 end
