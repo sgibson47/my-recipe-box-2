6ad982c383461070ce53b4e5aa3608345a1f95c7
sgibson47 <sgibson47@gmail.com>
2018-09-12 15:57:08 -0500

    define #ingredients_params

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -6,5 +6,12 @@ class IngredientsController < ApplicationController
   end
 
   def create
+    raise params.inspect
+  end
+
+  private
+
+  def ingredients_params
+    params.require(:ingredient).permit(:name, :serving_size_number, :serving_size_unit)
   end
 end
