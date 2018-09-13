43e2593435fe2fffd5202ff2eba32bd43b330cae
sgibson47 <sgibson47@gmail.com>
2018-09-13 11:25:44 -0500

    add validations to Recipe model ensuring that each recipe has a name and that each recipe's name is unique

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -3,4 +3,7 @@ class Recipe < ApplicationRecord
   has_many :amounts
   has_many :ingredients, :through => :amounts
   accepts_nested_attributes_for :amounts, reject_if: :all_blank
+
+  validates :name, presence: true
+  validates :name, uniqueness: true 
 end
