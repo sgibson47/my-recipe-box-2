7919db3c58f3feb4d009412f7a56ab624c14a50b
sgibson47 <sgibson47@gmail.com>
2018-09-18 13:16:05 -0500

    edit makings index action anticipating nested makings index that displays all the times a recipe has been made

--- app/controllers/makings_controller.rb
+++ app/controllers/makings_controller.rb
@@ -3,8 +3,8 @@ class MakingsController < ApplicationController
   def index
     if params[:user_id]
       @makings = User.find(params[:user_id]).makings
-    else
-      @makings = Making.all
+    elsif params[:recipe_id]
+      @makings = Recipe.find(params[:recipe_id]).makings
     end
   end
 
