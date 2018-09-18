e17621f33ab4816e9c02dc49bc0fb7685c71ea13
sgibson47 <sgibson47@gmail.com>
2018-09-18 13:13:50 -0500

    update ToDo

--- app/controllers/makings_controller.rb
+++ app/controllers/makings_controller.rb
@@ -1,3 +1,11 @@
 class MakingsController < ApplicationController
 
+  def index
+    if params[:user_id]
+      @makings = User.find(params[:user_id]).makings
+    else
+      @makings = Making.all
+    end
+  end
+
 end
