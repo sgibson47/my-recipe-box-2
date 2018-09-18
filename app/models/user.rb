bb56bc6ff8d2260a88c450bd72316f24992f1d03
sgibson47 <sgibson47@gmail.com>
2018-09-18 12:15:38 -0500

    add accepts_nested_attributes_for :recipes to user model to facilitate new recipe created nested under user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   has_secure_password
   has_many :recipes
+  accepts_nested_attributes_for :recipes, reject_if: :all_blank, :allow_destroy => true
 
   validates :name, :email, presence: true
   validates :password, confirmation: true
