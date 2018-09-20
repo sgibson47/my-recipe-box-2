caa132dab77b745cc02c521c7fe5b30b815a1ae3
sgibson47 <sgibson47@gmail.com>
2018-09-20 11:30:55 -0500

    add raise params.inspect to catch return from fb & see what we get

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,6 +6,7 @@ class SessionsController < ApplicationController
   end
 
   def create
+    raise param.inspect
     @user = User.find_by(email: params[:email])
     if @user && @user.authenticate(params[:password])
       session[:user_id] = @user.id
