986593a9e05f7efcd2ec67dc959a7c990bed2768
sgibson47 <sgibson47@gmail.com>
2018-09-20 10:52:30 -0500

    remove commented out unused code

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,26 +6,14 @@ class SessionsController < ApplicationController
   end
 
   def create
-    # @user = User.find_or_create_by(:email => auth['info']['email']) do |u|
-    # u.name = auth['info']['name']
-    # u.email = auth['info']['email']
-
-    # session[:user_id] = @user.id
-    # redirect_to user_path(@user)
-    # if params[:email]
-      @user = User.find_by(email: params[:email])
-      if @user && @user.authenticate(params[:password])
-        session[:user_id] = @user.id
-        redirect_to user_path(@user)
-      else
-        flash[:message] = "Invalid email and password combination."
-        redirect_to '/signin'
-      end
-    # elsif params[:provider]
-    #   @user = User.from_omniauth(reques.env["omniauth.auth"])
-    #   session[:user_id] = @user.id
-    #   redirect_to redirect_to user_path(@user)
-    # end
+    @user = User.find_by(email: params[:email])
+    if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      redirect_to user_path(@user)
+    else
+      flash[:message] = "Invalid email and password combination."
+      redirect_to '/signin'
+    end
   end
 
   def destroy
