059a1fbff133538a0be9b0cad0bf224b7bf984ca
sgibson47 <sgibson47@gmail.com>
2018-09-25 10:37:15 -0500

    move class method into scopes

--- app/models/making.rb
+++ app/models/making.rb
@@ -2,9 +2,7 @@ class Making < ApplicationRecord
   belongs_to :user
   belongs_to :recipe
 
-  def self.five_most_recent_makings
-    order(:created_at).limit(5)
-  end
-
+  scope :latest, -> { order(created_at: :desc) }
+  scope :five, -> { limit(5) }
 
 end
