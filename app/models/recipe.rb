b169ea960d1d3dd93325368a04a5d0212cc73d1b
sgibson47 <sgibson47@gmail.com>
2018-09-25 10:32:59 -0500

    move class methods into scopes

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -8,27 +8,20 @@ class Recipe < ApplicationRecord
   validates :name, presence: true
   validates :name, uniqueness: true 
 
+  scope :by_ingredient, -> (ingred_id) {includes(:ingredients).where(ingredients: {id: "#{ingred_id}"})}
   scope :by_avg_rating, -> { joins(:makings).group("recipes.id").order("avg(makings.rating)") }
   scope :by_times_made, -> { joins(:makings).group("recipes.id").order("count(makings.id) desc") }
-  
+  scope :latest, -> { order(created_at: :desc) }
+  scope :five, -> { limit(5) }
 
-  def self.five_most_recent_recipes
-    order(:created_at).limit(5)
-  end
+  
 
   def times_made
     makings.count
-  end
-
-  def self.by_ingredient(ingred_id)
-    includes(:ingredients).where(ingredients: {id: "#{ingred_id}"})
   end
 
   def avg_rating
     makings.average("rating").to_s
   end
 
-  def self.by_times_made
-    joins(:makings).group("recipes.id").order("count(makings.id) desc")
-  end
 end
