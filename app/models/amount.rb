75ca17b809e075e1f8e1491c67719b3b05fc0262
sgibson47 <sgibson47@gmail.com>
2018-09-25 16:06:41 -0500

    style recipe show page; define #ingred_for_display in amount model for use in recipe show page

--- app/models/amount.rb
+++ app/models/amount.rb
@@ -1,4 +1,10 @@
 class Amount < ApplicationRecord
   belongs_to :ingredient
   belongs_to :recipe
+
+  def ingred_for_display
+    num = self.servings_per_recipe * ingredient.serving_size_number 
+
+    "#{num} #{self.ingredient.serving_size_unit} #{self.ingredient.name}" 
+  end
 end
