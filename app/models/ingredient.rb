c88f4cb114c60f35ebc4e1994cea639eb34995c9
sgibson47 <sgibson47@gmail.com>
2018-09-25 14:58:01 -0500

    add validations to ingredient model

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -2,6 +2,10 @@ class Ingredient < ApplicationRecord
   has_many :amounts
   has_many :recipes, :through => :amounts
 
+  validates :name, :serving_size_number, :serving_size_unit, presence: true
+  validates :name, uniqueness: true 
+
+
   def name_with_serving_info
     "#{self.name} #{self.serving_size_number} #{self.serving_size_unit}"
   end
