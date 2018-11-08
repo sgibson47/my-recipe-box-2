694c5e491e5ad74c62dc5e1f25ecc38fc2797bcb
sgibson47 <sgibson47@gmail.com>
2018-11-08 12:25:39 -0600

    add function to create an empty collection of ingredients and an Ingredient class with a constructor function that gives each new JS object name, serving_size_number and  serving_size_unit properties whose values are set to the arguments passed in at the time the constructor is invoked with new and adds the new object to the INGREDIENTS collection

--- app/assets/javascripts/ingredients.js
+++ app/assets/javascripts/ingredients.js
@@ -2,3 +2,16 @@ $(document).ready(function(){
   // JS to be executed when page loads
 })
 
+function makeIngredientClass(){
+  let INGREDIENTS = []
+
+  class Ingredient{
+    constructor(name, serving_size_number, serving_size_unit){
+      this.name = name
+      this.serving_size_number = serving_size_number
+      this.serving_size_unit = serving_size_unit
+
+      INGREDIENTS.push(this)
+    }
+  }
+}
