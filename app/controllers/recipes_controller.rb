d0b1c5725fe09392f15350a73a8bcf5982453d71
sgibson47 <sgibson47@gmail.com>
2018-11-09 14:59:58 -0600

    refactor controller to pass the current user and an empty making associated with the recipe to the view

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -33,6 +33,8 @@ class RecipesController < ApplicationController
   end
 
   def show
+    @user = current_user
+    @making = @recipe.makings.build
     respond_to do |format|
       format.html { render :show }
       format.json { render json: @recipe, status: 200}
