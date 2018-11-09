53030baff49cd0b266da672ed7289c5b1fd87e65
sgibson47 <sgibson47@gmail.com>
2018-11-09 12:30:55 -0600

    add methods to Recipe class that create HTML of recipe's name, instructions, and div with link to edit form and to delete recipe

--- app/assets/javascripts/recipes.js
+++ app/assets/javascripts/recipes.js
@@ -10,6 +10,27 @@ document.addEventListener("turbolinks:load", function(){
       this.amounts = amounts
       this.ingredients = ingredients
     }
+
+    createRecipeHeader(){
+      return `<h1>${this.name}'s Page</h1>`
+    }
+    createInstructionsDiv(){
+      return `<div class="block instructions">
+        <h3>Instructions</h3>
+        <p>${this.instructions}</p>
+      </div><!--instructions-->`
+    }
+
+    createEditDeleteDiv(){
+      return `<div class="edit_delete">
+        <div class="edit block">
+          <a href="/recipes/${this.id}/edit">"Edit Recipe"</a>
+        </div><!--edit-->
+        <div class="delete block">
+          <a data-confirm="Are you sure?" data-method="delete" href="/recipes/${this.id}">"Delete Recipe"</a>
+        </div><!--delete-->
+      </div><!--edit_delete-->`
+    }
   }
 
   $.getJSON('/recipes/'+id, (recipe) => {
@@ -21,3 +42,12 @@ document.addEventListener("turbolinks:load", function(){
 
 })
 
+
+  
+  <div class="block ingred-list">
+    <h3>Ingredients:</h3>
+    <%= render "display_ingredients_for_recipe_show", recipe: @recipe %>
+  </div><!--ingred-ist-->
+
+
+  
