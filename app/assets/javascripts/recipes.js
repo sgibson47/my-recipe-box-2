cc5228c08625aa0485ed973b07d3c1ee6102029c
sgibson47 <sgibson47@gmail.com>
2018-11-09 11:04:38 -0600

    create assets/javascripts/recipes.js and add bits from ingredients.js to it

---
+++ app/assets/javascripts/recipes.js
@@ -0,0 +1,36 @@
+document.addEventListener("turbolinks:load", function(){
+
+  class Ingredient{
+    constructor(id, name, serving_size_number, serving_size_unit){
+      this.id = id
+      this.name = name
+      this.serving_size_number = serving_size_number
+      this.serving_size_unit = serving_size_unit
+    }
+
+    createIngredientDivs(){
+      return `<div class="name">
+        <a href="/ingredients/${this.id}">${this.name}</a>
+      </div><!--name-->
+      <div class="serving">
+        Serving Size: ${this.serving_size_number} ${this.serving_size_unit}
+      </div> <!--serving-->`
+    }
+  }
+
+  function displayIngredients(){
+    for(let i of INGREDIENTS.slice(start, end)){
+      $('#ingredients-index').append(i.createIngredientDivs())
+    }
+  }
+
+  $.getJSON('/ingredients', (ingredients) => {
+    ingredients.forEach( (i) => {
+      new Ingredient(i.id, i.name, i.serving_size_number, i.serving_size_unit)
+    })
+    displayIngredients();
+  });
+
+})
+
+
