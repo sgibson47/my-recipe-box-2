64179e9dd7534b7bfd5dccfd28123a3322aa53cc
sgibson47 <sgibson47@gmail.com>
2018-11-10 10:05:50 -0600

    move JS in makings.js into an event listener that listens for the page to load

--- app/assets/javascripts/makings.js
+++ app/assets/javascripts/makings.js
@@ -1,4 +1,12 @@
-$('#new_making_of_recipe').on('submit', function(e){
+document.addEventListener("turbolinks:load", function(){
+  $('#new_making_of_recipe').on('submit', function(e){
     e.preventDefault();
     console.log(this)
   })
+
+})
+
+
+
+
+  
