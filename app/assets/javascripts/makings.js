4950d6999d5b4ad1f2f00ec8dbdfa41ddd4a817c
sgibson47 <sgibson47@gmail.com>
2018-11-10 10:07:29 -0600

    add JS Making class

--- app/assets/javascripts/makings.js
+++ app/assets/javascripts/makings.js
@@ -1,4 +1,15 @@
 document.addEventListener("turbolinks:load", function(){
+  class Recipe{
+    constructor(id, rating, notes, user_id, recipe_id){
+      this.id = id
+      this.rating = rating
+      this.notes = notes
+      this.user_id = user_id
+      this.recipe_id = recipe_id
+    }
+
+  }
+
   $('#new_making_of_recipe').on('submit', function(e){
     e.preventDefault();
     console.log(this)
