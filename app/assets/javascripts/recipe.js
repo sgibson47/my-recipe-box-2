fa161551df84017c057c7da4eae3977fc267aa6a
sgibson47 <sgibson47@gmail.com>
2018-11-14 12:49:41 -0600

    refactor displaying makings

--- app/assets/javascripts/recipe.js
+++ app/assets/javascripts/recipe.js
@@ -1,64 +1,50 @@
-  let id = document.getElementById("recipe-show").dataset.id
-  let MAKINGS = []
-
-  class Recipe{
-    constructor(id, name, instructions, user_id, amounts, ingredients, makings){
-      this.id = id
-      this.name = name
-      this.instructions = instructions
-      this.user_id = user_id
-      this.amounts = amounts
-      this.ingredients = ingredients
-      this.makings = makings
-    }
-
-    createRecipeHeader(){
-      return `<h1>${this.name}'s Page</h1>`
-    }
-    createInstructionsDiv(){
-      return `<div class="block instructions">
-        <h3>Instructions</h3>
-        <p>${this.instructions}</p>
-      </div><!--instructions-->`
-    }
-
-    createEditDeleteDiv(){
-      return `<div class="edit_delete">
-        <div class="edit block">
-          <a href="/recipes/${this.id}/edit">Edit Recipe</a>
-        </div><!--edit-->
-        <div class="delete block">
-          <a data-confirm="Are you sure?" data-method="delete" href="/recipes/${this.id}">Delete Recipe</a>
-        </div><!--delete-->
-      </div><!--edit_delete-->`
-    }
+class Recipe{
+  constructor(id, name, instructions, user_id, amounts, ingredients, makings){
+    this.id = id
+    this.name = name
+    this.instructions = instructions
+    this.user_id = user_id
+    this.amounts = amounts
+    this.ingredients = ingredients
+    this.makings = makings
   }
 
-  class Making{
-    constructor(id, rating, notes, user_id, recipe_id){
-      this.id = id
-      this.rating = rating
-      this.notes = notes
-      this.user_id = user_id
-      this.recipe_id = recipe_id
-
-      MAKINGS.push(this)
-    }
-
-    createListItem(){
-      return `<li>Rating:  ${this.rating} stars <br>Notes: ${this.notes}</li>`
-    }
+  createRecipeHeader(){
+    return `<h1>${this.name}'s Page</h1>`
   }
 
-  function createIngredientDiv(){
-    return `<div class="block ingred-list">
-      <h3>Ingredients:</h3>
-      <ul>
-      </ul>
-    </div><!--ingred-ist-->`
+  createInstructionsDiv(){
+    return `<div class="block instructions">
+      <h3>Instructions</h3>
+      <p>${this.instructions}</p>
+    </div><!--instructions-->`
   }
 
-  function createIngredientListItems(amounts, ingredients){
+  createEditDeleteDiv(){
+    return `<div class="edit_delete">
+      <div class="edit block">
+        <a href="/recipes/${this.id}/edit">Edit Recipe</a>
+      </div><!--edit-->
+      <div class="delete block">
+        <a data-confirm="Are you sure?" data-method="delete" href="/recipes/${this.id}">Delete Recipe</a>
+      </div><!--delete-->
+    </div><!--edit_delete-->`
+  }
+
+  createMakingsDiv(){
+    return ` <div class="block instructions">
+      <h3>Times Users Made ${this.name}</h3>
+      <div id="recipe-makings" >
+        <ul>
+          ${createMakingListItems(this.makings)}
+        </ul>
+      </div>
+    </div>`
+  }
+}
+
+
+const createIngredientListItems = function (amounts, ingredients){
     var amountsArray =[]
     for(let a of amounts){
       amountsArray.push(a.servings_per_recipe)
@@ -74,50 +60,69 @@
     return listItems.join('')
   }
 
-  function createMakingListItems(makings){
-    var makingsListItemsToDisplay = []
-    makings.forEach(function(making, index, arr){
-      newMaking = new Making(making.id, making.rating, making.notes, making.user_id, making.recipe_id)
-      makingsListItemsToDisplay.push(newMaking.createListItem());
+const createMakingListItems = function (makings){
+    console.log(makings)
+    console.log(makings.length)
+    let makingsListItemsToDisplay = []
+    let makingsForEach = makings.slice(0, (makings.length-1))
+    makingsForEach.forEach(function(making, index, arr){
+      makingsListItemsToDisplay.push(`<li>Rating:  ${making.rating} stars <br>Notes: ${making.notes}</li>`);
     })
     return makingsListItemsToDisplay.join('')
   }
 
-  function displayIngredients(amounts, ingredients){
-    $('.ingred-list ul').append(createIngredientListItems(amounts, ingredients));
+const createIngredientDiv = function (amounts, ingredients){
+    return `<div class="block ingred-list">
+      <h3>Ingredients:</h3>
+      <ul>
+        ${createIngredientListItems(amounts, ingredients)}
+      </ul>
+    </div><!--ingred-list-->`
+
+    createIngredientListItems(amounts, ingredients)
   }
 
-  function displayMakings(makings){
-    var makingsToDisplay = makings.slice(0, (makings.length - 1))
-    $('#recipe-makings ul').append(createMakingListItems(makingsToDisplay));
-  }
+// const displayMakings = function (makings){
+//     console.log(makings)
+//     console.log(makings[0])
+//     let makingsToDisplayAfterSlice = makings.slice(0, (makings.length - 1))
+//     console.log(makingsToDisplayAfterSlice)
+//     let makingsHTML = createMakingListItems(makingsToDisplayAfterSlice);
+//     $('#recipe-makings ul').append(makingsHTML);
+//   }
 
-
-  function createShowHtml(recipe){
+const createShowHtml = function (recipe){
     let header = recipe.createRecipeHeader();
-    let ingredientsDiv = createIngredientDiv();
+    let ingredientsDiv = createIngredientDiv(recipe.amounts, recipe.ingredients);
     let instructions = recipe.createInstructionsDiv();
     let editDelete = recipe.createEditDeleteDiv();
+    let makings = recipe.createMakingsDiv();
 
-    return header + ingredientsDiv + instructions + editDelete
+    return header + ingredientsDiv + instructions + editDelete + makings
   }
 
+const getRecipe = function(){
+  let amountsToDisplay = [];
+  let ingredientsToDisplay = [];
+  let makingsToDisplay = [];
+  let id = $("#recipe-show").data('id');
 
   $.getJSON('/recipes/'+id, (recipe) => {
+
     $('#recipe-details').empty()
 
-    let newGuy = new Recipe(recipe.id, recipe.name, recipe.instructions, recipe.user_id, recipe.amounts, recipe.ingredients, recipe.makings);
+    newGuy = new Recipe(recipe.id, recipe.name, recipe.instructions, recipe.user_id, recipe.amounts, recipe.ingredients, recipe.makings);
 
     let html = createShowHtml(newGuy);
 
     $('#recipe-details').append(html);
-
-    displayIngredients(newGuy.amounts, newGuy.ingredients);
-
-    displayMakings(newGuy.makings);
   });
+}
 
 document.addEventListener("turbolinks:load", function(){
+
+  getRecipe();
+
   $('body').on('submit','#new_making_of_recipe', function(e){
     e.preventDefault();
 
